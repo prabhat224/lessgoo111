71ebe6d67e5a7d7805172dcc5592bc332a5d4ccd
Prabhat Dwivedi <prabhatdwivedi224@gmail.com>
2025-02-09 01:58:15 +0530

    This is the bas understanding

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
 public class Hello {
 
+    // public is the access modifier static is basically you are telling the compiler to load this method in the stack 
+    // void is the return type and String args are like default args to your main 
     public static void main(String args[])
     {
         System.out.println("This is the first code I ever wrote in java");
+        System.err.println(args);
     }
 }
