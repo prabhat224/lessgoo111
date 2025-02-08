54639c979a506b156a3332ff8f986c1dd6d137bb
Prabhat Dwivedi <prabhatdwivedi224@gmail.com>
2025-02-09 02:06:21 +0530

    This is the basics for data types

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello {
 
     // public is the access modifier static is basically you are telling the compiler to load this method in the stack 
     // void is the return type and String args are like default args to your main 
+    // int float double char  long --> data types you'll use 
+    // Integer String Long are classes that have predefined methods like : Integer.toString (pass some int value here);
     public static void main(String args[])
     {
         System.out.println("This is the first code I ever wrote in java");
